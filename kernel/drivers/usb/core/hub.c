39476850d535c512260753ec7a8a2292ac77889c
Jake Day <jake@ninebysix.com>
2017-08-21 22:28:27 -0400

    return out of usb_enable_lpm() if bos descriptors are null

--- kernel/drivers/usb/core/hub.c
+++ kernel/drivers/usb/core/hub.c
@@ -4044,7 +4044,8 @@ void usb_enable_lpm(struct usb_device *udev)
 	if (!udev || !udev->parent ||
 			udev->speed < USB_SPEED_SUPER ||
 			!udev->lpm_capable ||
-			udev->state < USB_STATE_DEFAULT)
+			udev->state < USB_STATE_DEFAULT ||
+			!udev->bos || !udev->bos->ss_cap)
 		return;
 
 	udev->lpm_disable_count--;
