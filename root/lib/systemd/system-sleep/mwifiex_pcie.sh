e6ec6b1cdb39b87d84b3771f79a1e26b0bf36c1d
Jake Day <jake@ninebysix.com>
2017-08-22 20:10:55 -0400

    updating wifi interface names

--- root/lib/systemd/system-sleep/mwifiex_pcie.sh
+++ root/lib/systemd/system-sleep/mwifiex_pcie.sh
@@ -6,12 +6,13 @@ set -eu
 
 case "$1" in
 pre)
-	ifdown --force mlan0 || true
+	ifdown --force wlp3s0 || true
 	modprobe -r mwifiex_pcie
 	;;
 post)
 	echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
 	modprobe mwifiex_pcie
+	ifup --force wlp3s0 || true
 	;;
 esac
 
