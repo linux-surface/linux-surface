a2e1d275091fe16331b59de2e54abe50d774aeae
Jake Day <jake@ninebysix.com>
2017-09-06 20:25:47 -0400

    update script for reloading mwifiex_pice module on wake

--- root/lib/systemd/system-sleep/mwifiex_pcie.sh
+++ root/lib/systemd/system-sleep/mwifiex_pcie.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
-#
-# mwifiex_pcie: handle the flakey wifi
+set -e
 
-set -eu
-
-case "$1" in
-pre)
-	ifdown --force wlp3s0 || true
-	modprobe -r mwifiex_pcie
-	;;
-post)
-	echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
-	modprobe mwifiex_pcie
-	ifup --force wlp3s0 || true
-	;;
-esac
-
-exit 0
+if [ "$2" = "hibernate" ]; then
+    case "$1" in
+        pre) modprobe -r mwifiex_pcie mwifiex ;;
+        post) modprobe mwifiex_pcie ;;
+    esac
