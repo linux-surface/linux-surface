c6f6880e52ca80d99f1ce52e838922847f59b00d
Jake Day <jake@ninebysix.com>
2017-11-17 16:09:28 -0500

    re-enable power save mode on wifi module

--- kernel/drivers/net/wireless/marvell/mwifiex/cfg80211.c
+++ kernel/drivers/net/wireless/marvell/mwifiex/cfg80211.c
@@ -416,9 +416,6 @@ mwifiex_cfg80211_set_power_mgmt(struct wiphy *wiphy,
 
 	ps_mode = enabled;
 
-	mwifiex_dbg(priv->adapter, ERROR, "overriding ps_mode to false\n");
-	ps_mode = 0;
-
 	return mwifiex_drv_set_power(priv, &ps_mode);
 }
 
--- kernel/drivers/net/wireless/marvell/mwifiex/sta_cmd.c
+++ kernel/drivers/net/wireless/marvell/mwifiex/sta_cmd.c
@@ -2313,7 +2313,7 @@ int mwifiex_sta_init_cmd(struct mwifiex_private *priv, u8 first_sta, bool init)
 		if (ret)
 			return -1;
 
-		if (0 && priv->bss_type != MWIFIEX_BSS_TYPE_UAP) {
+		if (priv->bss_type != MWIFIEX_BSS_TYPE_UAP) {
 			/* Enable IEEE PS by default */
 			priv->adapter->ps_mode = MWIFIEX_802_11_POWER_MODE_PSP;
 			ret = mwifiex_send_cmd(priv,
@@ -2369,7 +2369,7 @@ int mwifiex_sta_init_cmd(struct mwifiex_private *priv, u8 first_sta, bool init)
 	if (ret)
 		return -1;
 
-	if (0 && !disable_auto_ds && first_sta &&
+	if (!disable_auto_ds && first_sta &&
 	    priv->bss_type != MWIFIEX_BSS_TYPE_UAP) {
 		/* Enable auto deep sleep */
 		auto_ds.auto_ds = DEEP_SLEEP_ON;
