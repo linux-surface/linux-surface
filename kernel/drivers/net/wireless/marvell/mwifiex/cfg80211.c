2ff5be2f278e5dd24b7953ae1af5d209e70ad2ec
Jake Day <jake@ninebysix.com>
2017-11-21 13:27:53 -0500

    Revert "re-enable power save mode on wifi module"
    This reverts commit c6f6880e52ca80d99f1ce52e838922847f59b00d.

--- kernel/drivers/net/wireless/marvell/mwifiex/cfg80211.c
+++ kernel/drivers/net/wireless/marvell/mwifiex/cfg80211.c
@@ -416,6 +416,9 @@ mwifiex_cfg80211_set_power_mgmt(struct wiphy *wiphy,
 
 	ps_mode = enabled;
 
+	mwifiex_dbg(priv->adapter, ERROR, "overriding ps_mode to false\n");
+	ps_mode = 0;
+
 	return mwifiex_drv_set_power(priv, &ps_mode);
 }
 
