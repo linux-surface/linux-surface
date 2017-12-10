61c6bf8a73d726cdae776f021e235d8a26d6fa04
Jake Day <jake@ninebysix.com>
2017-12-10 12:46:24 -0500

    resets are not needed for hibernate

--- root/lib/systemd/system-sleep/hibernate.sh
+++ root/lib/systemd/system-sleep/hibernate.sh
@@ -7,12 +7,12 @@ case $1/$2 in
     rmmod mei
     rmmod mwifiex_pcie
     rmmod mwifiex
-    echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
+    #echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
     ;;
   post/*)
     modprobe mei
     modprobe mei_me
-    echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
+    #echo 1 > /sys/bus/pci/devices/0000\:02\:00.0/reset
     modprobe mwifiex
     modprobe mwifiex_pcie
     systemctl start NetworkManager.service
