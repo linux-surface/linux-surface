ad5012c900ca1cd62c4282067b2edb75301697fd
kitakar5525 <34676735+kitakar5525@users.noreply.github.com>
2019-07-31 05:27:38 +0900

    Update NVMe part of suspend.patch
    Note:
    NVMe part will be merged into Linux 5.3. Remove the part in
    0002-suspend.patch when it arrives.
    
    For 5.2
    - (Reverted NVMe part of 0002-suspend.patch to apply below patch set)
    - nvme: export get and set features · torvalds/linux@1a87ee6
    https://github.com/torvalds/linux/commit/1a87ee657c530bb2f3e39e4ac184d48f5f959cda
    - nvme-pci: use host managed power state for suspend · torvalds/linux@d916b1b
    https://github.com/torvalds/linux/commit/d916b1be94b6dc8d293abed2451f3062f6af7551#diff-bc4c090f021c046a7d256a3fcf86b7da
    
    For 4.19, this patch is also applied
    - nvme-pci: Sync queues on reset · torvalds/linux@d6135c3
    https://github.com/torvalds/linux/commit/d6135c3a1ec0cddda7b8b8e1b5b4abeeafd98289#diff-bc4c090f021c046a7d256a3fcf86b7da
    
    See
    - Surface Book with Performance Base: NVMe SSD breaks suspend (s2idle) · Issue #123 · jakeday/linux-surface
    https://github.com/jakeday/linux-surface/issues/123

---
+++ README.md
@@ -0,0 +1,147 @@
+# Linux Surface
+
+Linux running on the Microsoft Surface devices . Follow the instructions below to install the latest kernel and config files.
+
+### Supported Devices
+
+* Surface Book
+* Surface Book 2
+* Surface Go
+* Surface Laptop
+* Surface Laptop 2
+* Surface Pro 3
+* Surface Pro 4
+* Surface Pro 2017
+* Surface Pro 6
+* Surface Studio
+
+### What's Working
+
+* Keyboard (and backlight)
+* Touchpad
+* 2D/3D Acceleration
+* Touchscreen
+* Pen
+* WiFi
+* Bluetooth
+* Speakers
+* Power Button
+* Volume Buttons
+* SD Card Reader
+* Cameras (partial support, disabled for now)
+* Hibernate
+* Sensors (accelerometer, gyroscope, ambient light sensor)
+* Battery Readings
+* Docking/Undocking Tablet and Keyboard
+* Surface Docks
+* DisplayPort
+* USB-C (including for HDMI Out)
+* Dedicated Nvidia GPU (Surface Book 2)
+
+### What's NOT Working
+
+* Dedicated Nvidia GPU (if you have a performance base on a Surface Book 1, otherwise onboard works fine)
+* Cameras (not fully supported yet)
+* Connected Standby is not supported yet
+
+### Disclaimer
+* For the most part, things are tested on a Surface Book. While most things are reportedly fully working on other devices, your mileage may vary. Please look at the issues list for possible exceptions.
+
+### Download Pre-built Kernel and Headers
+
+The setup.sh script in the below Instructions will handle downloading these for you. Otherwise, you may still download them directly (not recommended as you will want to run the setup.sh script).
+
+Downloads for ubuntu based distros (other distros will need to compile from source using the included patches):
+
+https://github.com/jakeday/linux-surface/releases
+
+You will need to download the image, headers and libc-dev deb files for the version you want to install.
+
+### Instructions
+
+0. (Prep) Install Dependencies:
+  ```
+   sudo apt install git curl wget sed
+  ```
+1. Clone the linux-surface repo:
+  ```
+   git clone --depth 1 https://github.com/jakeday/linux-surface.git ~/linux-surface
+  ```
+2. Change directory to linux-surface repo:
+  ```
+   cd ~/linux-surface
+  ```
+3. Run setup script:
+  ```
+   sudo sh setup.sh
+  ```
+4. Reboot on installed kernel.
+
+The setup script will handle installing the latest kernel for you. You can also choose to download any version you want and install yourself:
+Install the headers, kernel and libc-dev (make sure you cd to your download location first):
+  ```
+  sudo dpkg -i linux-headers-[VERSION].deb linux-image-[VERSION].deb linux-libc-dev-[VERSION].deb
+  ```
+
+### Compiling the Kernel from Source
+
+#### For Debian-Based Systems
+
+If you don't want to use the pre-built kernel and headers, you can compile the kernel yourself following these steps:
+
+0. (Prep) Install the required packages for compiling the kernel:
+  ```
+  sudo apt install build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev
+  ```
+1. Clone the mainline stable kernel repo:
+  ```
+  git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git ~/linux-stable
+  ```
+2. Go into the linux-stable directory:
+  ```
+  cd ~/linux-stable
+  ```
+3. Checkout the version of the kernel you wish to target (replacing with your target version):
+  ```
+  git checkout v5.y.z
+  ```
+4. Apply the kernel patches from the linux-surface repo (this one, and assuming you cloned it to ~/linux-surface):
+  ```
+  for i in ~/linux-surface/patches/[VERSION]/*.patch; do patch -p1 < $i; done
+  ```
+5. Use config for kernel series (may need to manually change for your distro):
+  ```
+  cp ~/linux-surface/configs/[VERSION]/config .config
+  ```
+6. Compile the kernel and headers (for ubuntu, refer to the build guide for your distro):
+  ```
+  make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-linux-surface
+  ```
+7. Install the headers, kernel and libc-dev:
+  ```
+  sudo dpkg -i linux-headers-[VERSION].deb linux-image-[VERSION].deb linux-libc-dev-[VERSION].deb
+  ```
+
+#### For Arch-Based Systems
+
+Have a look at [this](https://github.com/dmhacker/arch-linux-surface) repository.
+
+### Signing the kernel for Secure Boot
+
+Please consult the [SIGNING.md](SIGNING.md).
+
+### NOTES
+
+* If you are getting stuck at boot when loading the ramdisk, you need to install the Processor Microcode Firmware for Intel CPUs (usually found under Additional Drivers in Software and Updates).
+* Do not install TLP! It can cause slowdowns, laggy performance, and occasional hangs! You have been warned.
+* If you chose to use hibernate over suspend, please follow the instructions [here](https://fitzcarraldoblog.wordpress.com/2018/07/14/configuring-lubuntu-18-04-to-enable-hibernation-using-a-swap-file/).
+
+### Support
+
+If you have an issue with the kernel, please feel free to create on issue here to track it. If you have questions or need support, please use our [Gitter Community](https://gitter.im/linux-surface)!
+
+### Donations Appreciated!
+
+PayPal: https://www.paypal.me/jakeday42
+
+Bitcoin: 1AH7ByeJBjMoAwsgi9oeNvVLmZHvGoQg68
---
+++ setup.sh
@@ -0,0 +1,246 @@
+#!/bin/sh
+
+LX_BASE=""
+LX_VERSION=""
+
+if [ -r /etc/os-release ]; then
+    . /etc/os-release
+	if [ $ID = arch ]; then
+		LX_BASE=$ID
+    elif [ $ID = ubuntu ]; then
+		LX_BASE=$ID
+		LX_VERSION=$VERSION_ID
+	elif [ ! -z "$UBUNTU_CODENAME" ] ; then
+		LX_BASE="ubuntu"
+		LX_VERSION=$VERSION_ID
+    else
+		LX_BASE=$ID
+		LX_VERSION=$VERSION
+    fi
+else
+    echo "Could not identify your distro. Please open script and run commands manually."
+	exit
+fi
+
+SUR_MODEL="$(dmidecode | grep "Product Name" -m 1 | xargs | sed -e 's/Product Name: //g')"
+SUR_SKU="$(dmidecode | grep "SKU Number" -m 1 | xargs | sed -e 's/SKU Number: //g')"
+
+echo "\nRunning $LX_BASE version $LX_VERSION on a $SUR_MODEL.\n"
+
+read -rp "Press enter if this is correct, or CTRL-C to cancel." cont;echo
+
+echo "\nContinuing setup...\n"
+
+echo "Copying the config files under root to where they belong...\n"
+for dir in $(ls root/); do cp -Rb root/$dir/* /$dir/; done
+
+echo "Making /lib/systemd/system-sleep/sleep executable...\n"
+chmod a+x /lib/systemd/system-sleep/sleep
+
+echo "Suspend is recommended over hibernate. If you chose to use hibernate, please make sure you've setup your swap file per the instructions in the README.\n"
+read -rp "Do you want to replace suspend with hibernate? (type yes or no) " usehibernate;echo
+
+if [ "$usehibernate" = "yes" ]; then
+	if [ "$LX_BASE" = "ubuntu" ] && [ 1 -eq "$(echo "${LX_VERSION} >= 17.10" | bc)" ]; then
+		echo "Using Hibernate instead of Suspend...\n"
+		ln -sfb /lib/systemd/system/hibernate.target /etc/systemd/system/suspend.target && sudo ln -sfb /lib/systemd/system/systemd-hibernate.service /etc/systemd/system/systemd-suspend.service
+	else
+		echo "Using Hibernate instead of Suspend...\n"
+		ln -sfb /usr/lib/systemd/system/hibernate.target /etc/systemd/system/suspend.target && sudo ln -sfb /usr/lib/systemd/system/systemd-hibernate.service /etc/systemd/system/systemd-suspend.service
+	fi
+else
+	echo "Not touching Suspend\n"
+fi
+
+echo "Patched libwacom packages are available to better support the pen. If you intend to use the pen, it's recommended that you install them!\n"
+
+read -rp "Do you want to install the patched libwacom packages? (type yes or no) " uselibwacom;echo
+
+if [ "$uselibwacom" = "yes" ]; then
+	echo "Installing patched libwacom packages..."
+		dpkg -i packages/libwacom/*.deb
+		apt-mark hold libwacom
+else
+	echo "Not touching libwacom"
+fi
+
+echo "This repo comes with example xorg and pulse audio configs. If you chose to keep them, be sure to rename them and uncomment out what you'd like to keep!\n"
+
+read -rp "Do you want to remove the example intel xorg config? (type yes or no) " removexorg;echo
+
+if [ "$removexorg" = "yes" ]; then
+	echo "Removing the example intel xorg config..."
+		rm /etc/X11/xorg.conf.d/20-intel_example.conf
+else
+	echo "Not touching example intel xorg config (/etc/X11/xorg.conf.d/20-intel_example.conf)"
+fi
+
+read -rp "Do you want to remove the example pulse audio config files? (type yes or no) " removepulse;echo
+
+if [ "$removepulse" = "yes" ]; then
+	echo "Removing the example pulse audio config files..."
+		rm /etc/pulse/daemon_example.conf
+		rm /etc/pulse/default_example.pa
+else
+	echo "Not touching example pulse audio config files (/etc/pulse/*_example.*)"
+fi
+
+if [ "$SUR_MODEL" = "Surface Pro 3" ]; then
+	echo "\nInstalling i915 firmware for Surface Pro 3...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_bxt.zip -d /lib/firmware/i915/
+
+	echo "\nRemove unneeded udev rules for Surface Pro 3...\n"
+	rm /etc/udev/rules.d/98-keyboardscovers.rules
+fi
+
+if [ "$SUR_MODEL" = "Surface Pro" ]; then
+	echo "\nInstalling IPTS firmware for Surface Pro 2017...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v102.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Pro 2017...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Pro 4" ]; then
+	echo "\nInstalling IPTS firmware for Surface Pro 4...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v78.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Pro 4...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_skl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Pro 2017" ]; then
+	echo "\nInstalling IPTS firmware for Surface Pro 2017...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v102.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Pro 2017...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Pro 6" ]; then
+	echo "\nInstalling IPTS firmware for Surface Pro 6...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v102.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Pro 6...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Studio" ]; then
+	echo "\nInstalling IPTS firmware for Surface Studio...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v76.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Studio...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_skl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Laptop" ]; then
+	echo "\nInstalling IPTS firmware for Surface Laptop...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v79.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Laptop...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Laptop 2" ]; then
+	echo "\nInstalling IPTS firmware for Surface Laptop 2...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v79.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Laptop 2...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Book" ]; then
+	echo "\nInstalling IPTS firmware for Surface Book...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	unzip -o firmware/ipts_firmware_v76.zip -d /lib/firmware/intel/ipts/
+
+	echo "\nInstalling i915 firmware for Surface Book...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_skl.zip -d /lib/firmware/i915/
+fi
+
+if [ "$SUR_MODEL" = "Surface Book 2" ]; then
+	echo "\nInstalling IPTS firmware for Surface Book 2...\n"
+	mkdir -p /lib/firmware/intel/ipts
+	if [ "$SUR_SKU" = "Surface_Book_1793" ]; then
+		unzip -o firmware/ipts_firmware_v101.zip -d /lib/firmware/intel/ipts/
+	else
+		unzip -o firmware/ipts_firmware_v137.zip -d /lib/firmware/intel/ipts/
+	fi
+
+	echo "\nInstalling i915 firmware for Surface Book 2...\n"
+	mkdir -p /lib/firmware/i915
+	unzip -o firmware/i915_firmware_kbl.zip -d /lib/firmware/i915/
+
+	echo "\nInstalling nvidia firmware for Surface Book 2...\n"
+	mkdir -p /lib/firmware/nvidia/gp108
+	unzip -o firmware/nvidia_firmware_gp108.zip -d /lib/firmware/nvidia/gp108/
+	mkdir -p /lib/firmware/nvidia/gv100
+	unzip -o firmware/nvidia_firmware_gv100.zip -d /lib/firmware/nvidia/gv100/
+fi
+
+if [ "$SUR_MODEL" = "Surface Go" ]; then
+	echo "\nInstalling ath10k firmware for Surface Go...\n"
+	mkdir -p /lib/firmware/ath10k
+	unzip -o firmware/ath10k_firmware.zip -d /lib/firmware/ath10k/
+
+	if [ ! -f "/etc/init.d/surfacego-touchscreen" ]; then
+		echo "\nPatching power control for Surface Go touchscreen...\n"
+		echo "echo \"on\" > /sys/devices/pci0000:00/0000:00:15.1/i2c_designware.1/power/control" > /etc/init.d/surfacego-touchscreen
+		chmod 755 /etc/init.d/surfacego-touchscreen
+		update-rc.d surfacego-touchscreen defaults
+	fi
+fi
+
+echo "Installing marvell firmware...\n"
+mkdir -p /lib/firmware/mrvl/
+unzip -o firmware/mrvl_firmware.zip -d /lib/firmware/mrvl/
+
+echo "Installing mwlwifi firmware...\n"
+mkdir -p /lib/firmware/mwlwifi/
+unzip -o firmware/mwlwifi_firmware.zip -d /lib/firmware/mwlwifi/
+
+read -rp "Do you want to set your clock to local time instead of UTC? This fixes issues when dual booting with Windows. (type yes or no) " uselocaltime;echo
+
+if [ "$uselocaltime" = "yes" ]; then
+	echo "Setting clock to local time...\n"
+
+	timedatectl set-local-rtc 1
+	hwclock --systohc --localtime
+else
+	echo "Not setting clock"
+fi
+
+read -rp "Do you want this script to download and install the latest kernel for you? (type yes or no) " autoinstallkernel;echo
+
+if [ "$autoinstallkernel" = "yes" ]; then
+	echo "Downloading latest kernel...\n"
+
+	urls=$(curl --silent "https://api.github.com/repos/jakeday/linux-surface/releases/latest" | tr ',' '\n' | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+	resp=$(wget -P tmp $urls)
+
+	echo "Installing latest kernel...\n"
+
+	dpkg -i tmp/*.deb
+	rm -rf tmp
+else
+	echo "Not downloading latest kernel"
+fi
+
+echo "\nAll done! Please reboot."
