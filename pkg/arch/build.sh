ee24ddfb6daf60ba6ec81fdb7434821fb63c0001
Blaž Hrastnik <blaz@mxxn.io>
2020-01-10 16:21:56 +0900

    Concurrency all the things!

--- pkg/arch/build.sh
+++ pkg/arch/build.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 # Package compression settings (Matches latest Arch)
 export PKGEXT='.pkg.tar.zst'
 export COMPRESSZST=(zstd -c -T0 --ultra -20 -)
+export MAKEFLAGS="-j2"
 
 # Build the packages
 pushd surface-ipts-firmware
