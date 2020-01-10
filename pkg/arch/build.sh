5daed056a362f001d89d82935b2b16eee757c744
Blaž Hrastnik <blaz@mxxn.io>
2020-01-10 10:25:28 +0900

    Update the workflow, split into multiple steps.

--- pkg/arch/build.sh
+++ pkg/arch/build.sh
@@ -5,19 +5,11 @@ set -euxo pipefail
 export PKGEXT='.pkg.tar.zst'
 export COMPRESSZST=(zstd -c -T0 --ultra -20 -)
 
-# Import GPG key
-echo "$GPG_KEY" | base64 -d | gpg --import --no-tty --batch --yes
-export GPG_TTY=$(tty)
-
-# Build the packages as `build' user
+# Build the packages
 pushd surface-ipts-firmware
 makepkg -f --syncdeps --skippgpcheck --noconfirm
-# Sign as a separate step (makepkg -s needs pinentry)
-makepkg --packagelist | xargs -L1 gpg --detach-sign --batch --no-tty --pinentry-mode=loopback --passphrase $GPG_PASSPHRASE -u 5B574D1B513F9A05
 popd
 
 pushd kernel
 makepkg -f --syncdeps --skippgpcheck --noconfirm
-# Sign as a separate step (makepkg -s needs pinentry)
-makepkg --packagelist | xargs -L1 gpg --detach-sign --batch --no-tty --pinentry-mode=loopback --passphrase $GPG_PASSPHRASE -u 5B574D1B513F9A05
 popd
