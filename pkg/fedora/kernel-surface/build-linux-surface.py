b271d3713034784821d4d9e1ac75a92c3f10a6b6
Dorian Stoll <dorian.stoll@tmsp.io>
2023-12-08 17:26:30 +0100

    pkg: fedora: Update to kernel 6.6.5

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -18,13 +18,13 @@ PACKAGE_NAME = "surface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-6.5.11-0"
+PACKAGE_TAG = "kernel-6.6.5-0"
 
 ##
 ## The release number of the modified kernel package.
 ## e.g. 300 for kernel-6.3.1-300.fc38.foo
 ##
-PACKAGE_RELEASE = "2"
+PACKAGE_RELEASE = "1"
 
 ##
 ## Build options for configuring which parts of the kernel package are enabled.
