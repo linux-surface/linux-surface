94217c2dc8818afd2296c3776223fc1c093f78fb
Maximilian Luz <luzmaximilian@gmail.com>
2025-09-30 20:24:23 +0200

    Update Fedora kernels to v6.16.9

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -19,7 +19,7 @@ PACKAGE_NAME = "surface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-6.15.3-0"
+PACKAGE_TAG = "kernel-6.16.9-0"
 
 ##
 ## The release number of the modified kernel package.
